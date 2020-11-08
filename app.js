8731328d2611f0df43ca72758fa941ebec5fa32e
Alka Muneesamy <alkak22@yahoo.com>
2020-11-08 21:59:44 +0000

    Adding a Procfile

--- app.js
+++ app.js
@@ -219,7 +219,10 @@ app.post("/failure", function(req, res) {
 //end of login for newsletter sign up
 
 
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is running on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server has started successfully!!!");
 });
